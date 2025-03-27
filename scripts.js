30a87996aef6259bb9dc3473d098c7b74752beea
mariani0305 <134952888+mariani0305@users.noreply.github.com>
2025-03-27 11:15:40 +0000

    arrumar botões acessibilidades

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const botaoAumentarFonte = document.getElementById('aumentar-fonte')
+    const botaoDiminuirFonte = document.getElementById('diminuir-fonte')
     //Pega o botão de aumentar fonte e coloca na constante
 
     let tamanhoDaFonte = 1
     // define o tamanho da fonte na variável
 
-    botaoAumentarFonte.addEventListener('click', function(){
+    botaoDiminuirFonte.addEventListener('click', function(){
         tamanhoDaFonte = tamanhoDaFonte + 0.1
         document.body.style.fontSize = `${tamanhoDaFonte}rem`
     })
