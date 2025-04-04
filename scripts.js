75c875039676ef0d876f8aca32318bf531e654b7
mariani0305 <134952888+mariani0305@users.noreply.github.com>
2025-04-04 12:56:27 +0000

    codigo

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const botaoDiminuirFonte = document.getElementById('diminuir-fonte')
+    const botaoAumentarFonte = document.getElementById('aumentar-fonte')
     //Pega o botão de aumentar fonte e coloca na constante
 
     let tamanhoDaFonte = 1
     // define o tamanho da fonte na variável
 
-    botaoDiminuirFonte.addEventListener('click', function(){
-        tamanhoDaFonte = tamanhoDaFonte + 0.1
-        document.body.style.fontSize = `${tamanhoDaFonte}rem`
+    botaoAumentarFonte.addEventListener('click', function(){
+        tamanhoDaFonte = tamanhoDaFonte + 0.1;
+        document.body.style.fontSize = `${tamanhoDaFonte}rem`;
+    })
+
+    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById ('opcoes-acessibilidade');
+    
+    botaoAcessibilidade.addEventListener('click', function(){
+        botaoAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista')
     })
 
 
